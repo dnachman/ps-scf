2fad37b0ba6965034d768149ae8aefdc786d92e8
David Nachman <david@nachman.org>
2018-10-21 16:40:44 -0400

    three new projects getting ready for routing

---
+++ goodbye-service/src/main/java/com/logicalenigma/psscf/goodbyeservice/GoodbyeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.logicalenigma.psscf.goodbyeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodbyeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodbyeServiceApplication.class, args);
+	}
+}
